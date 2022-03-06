5297307164b141ef5f4514c68729cb646073245f
Chris Jonak <chris@ironbrush.com>
2022-03-06 12:05:00 -0600

    Created random array/word function for computer/Created function to run a round of play

---
+++ main.js
@@ -0,0 +1,30 @@
+
+const words = ["Rock", "Paper", "Scissors"];
+
+function computerPlay() {
+   return words[Math.floor(Math.random() * words.length)]; /* 1. words is an array so [] go
+   after words. 2.Math.random = random number between 0 and 1, Math.floor rounds down to whole
+   number to create a whole integer. 3.Math.random() * words.length goes first THEN rounds down 
+   to a whole integer! */
+}
+
+
+function playRound(playerSelection, computerSelection) {
+    let playerSelection = prompt "Type Rock, Paper or Scissors";
+    input = playerSelection.toLowerCase(); // Makes any typed selection non-case sensitive
+
+    if(playerSelection === computerSelection) {
+        return "Tie!";
+    } else if((playerSelection === "rock" && computerSelection === "scissors") || 
+    (playerSelection === "scissors" && computerSelection === "paper") ||
+    (playerSelection === "paper" && computerSelection === "rock")) {
+       return `${playerSelection} beats ${computerSelection}, you win!`;
+    } else {
+        return `${computerSelection} beats ${playerSelection}, you lose!`;
+    }
+}
+
+
+    const playerSelection = "Rock";
+    const computerSelection = computerPlay();
+    console.log(playRound(playerSelection, computerSelection));
