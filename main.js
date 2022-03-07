281410b69f14841a3b036ee66b0e28cf3340cb29
Chris Jonak <chris@ironbrush.com>
2022-03-07 11:18:19 -0600

    created function game(), and moving function playRound() inside. Also figuring out the for loop to loop through 5 playthroughs.

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 
-const words = ["rock", "paper", "scissors"]; // <----This function works ****
+const words = ["rock", "paper", "scissors"]; 
 
 function computerPlay() {
    return words[Math.floor(Math.random() * words.length)]; /* 1. words is an array so [] go
@@ -8,23 +8,36 @@ function computerPlay() {
    to a whole integer! */
 }
 
+const computerSelection = computerPlay(); 
 
-function playRound(playerSelection, computerSelection) {
+function game() { 
     let input = prompt ("Type Rock, Paper or Scissors");
-    playerSelection = input.toLowerCase(); // Makes any typed selection non-case sensitive
+            playerSelection = input.toLowerCase(); // Makes any typed selection non-case sensitive
+    for (let i = 0; i < 5; i++) {
+    let gameRounds = i;
+    let playerWin = 0;
+    let computerWin = 0;
+    
+        function playRound(playerSelection, computerSelection) {
+        
+            if(playerSelection === computerSelection) {
+                return "Tie!";
+            } else if((playerSelection === "rock" && computerSelection === "scissors") || 
+            (playerSelection === "scissors" && computerSelection === "paper") ||
+            (playerSelection === "paper" && computerSelection === "rock")) {
+               return `${playerSelection} beats ${computerSelection}, you win!`;
+               playerWin++;
+            } else {
+                return `${computerSelection} beats ${playerSelection}, you lose!`;
+                computerWin++;
+            }
+        }
 
-    if(playerSelection === computerSelection) {
-        return "Tie!";
-    } else if((playerSelection === "rock" && computerSelection === "scissors") || 
-    (playerSelection === "scissors" && computerSelection === "paper") ||
-    (playerSelection === "paper" && computerSelection === "rock")) {
-       return `${playerSelection} beats ${computerSelection}, you win!`;
-    } else {
-        return `${computerSelection} beats ${playerSelection}, you lose!`;
+    }
+    
+    if(playerWin === 5) {
+        console.log(`You won the game with ${playerWin} points!`)
+    } else if(computerWin === 5) {
+        console.log(`You lost the game! Computer ${computerWin} points, beats your ${playerWin} points!`)
     }
 }
-
-const playerSelection = "rock";           // <-----Have to call this three line block to test App.
-const computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection));
-
