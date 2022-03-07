4433c47a9d77bf60738b2708014b68c3ce8d9abb
Chris Jonak <chris@ironbrush.com>
2022-03-07 14:36:15 -0600

    game(); works but now computerSeletion is being listed as undefined...

--- main.js
+++ main.js
@@ -2,11 +2,12 @@
 
 let playerWin = 0;
 let computerWin = 0;
-const computerSelection = computerPlay(); 
+
+
 
 const words = ["rock", "paper", "scissors"]; 
-
 function computerPlay() {
+    
    return words[Math.floor(Math.random() * words.length)]; /* 1. words is an array so [] go
    after words. 2.Math.random = random number between 0 and 1, Math.floor rounds down to whole
    number to create a whole integer. 3.Math.random() * words.length goes first THEN rounds down 
@@ -20,31 +21,29 @@ function playRound(playerSelection, computerSelection) {
     playerSelection = input.toLowerCase(); // Makes any typed selection non-case sensitive
         
     if(playerSelection === computerSelection) {
-        return "Tie!";
+        console.log("Tie!");
     } else if((playerSelection === "rock" && computerSelection === "scissors") || 
     (playerSelection === "scissors" && computerSelection === "paper") ||
     (playerSelection === "paper" && computerSelection === "rock")) {
         playerWin++;
-       return `${playerSelection} beats ${computerSelection}, you win!`;
+       console.log(`${playerSelection} beats ${computerSelection}, you win!`);
     } else {
         computerWin++;
-        return `${computerSelection} beats ${playerSelection}, you lose!`;
-        
+        console.log(`${computerSelection} beats ${playerSelection}, you lose!`);   
     }
 }
 
 function game() { 
-    for (let i = 0; i <= 5; i++) {
+    for (let i = 0; i < 5; i++) {
     playRound();
-    let gameRounds = i;
-    }
-    
     if(playerWin === 5) {
-        console.log(`You won the game with ${playerWin} points!`)
+        console.log(`You won the game with ${playerWin} points!`);
     } else if(computerWin === 5) {
-        console.log(`You lost the game! Computer ${computerWin} points, beats your ${playerWin} points!`)
+        console.log(`You lost the game! Computer: ${computerWin} points, beats your ${playerWin} points!`);
     }
 }
+}
 
+const computerSelection = computerPlay(); 
 
-game();
+console.log(game());
