eca01b24d3305a8a38ff8e987607f2714b77e117
Chris Jonak <chris@ironbrush.com>
2022-05-18 13:11:38 -0500

    added results div and first JS DOM inserts

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+/* The Odin Project - Rock Paper Scissors Part 1 and Part 2 */
+
+
+/* Most of the commented out pieces are from the first part of Odin Project's assignment for RPS. The rest are 
+comments for myself to remember how some syntax works. */
 
 
 let playerWin = 0;
@@ -25,11 +30,26 @@ function computerPlay() {
 
 
 function playRound(playerSelection, computerSelection) {
-    console.log(`Player's score is ${playerWin} and Computer's score is ${computerWin}`);
+    
+    
+    //console.log(`Player's score is ${playerWin} and Computer's score is ${computerWin}`);
     //let input = prompt ("Type Rock, Paper or Scissors"); <----- input prompt for console play!
     //let playerSelection = btnRock.value;  <----grabs the value/inner html of what is inside the button.
     //let playerSelection = input.toLowerCase(); <------- Makes any typed selection non-case sensitive
     //let computerSelection = computerPlay();
+
+const results = document.getElementById('results');
+
+const tally = document.createElement('p');
+tally.classList.add('tally');
+tally.textContent = `Player's score is ${playerWin} and Computer's score is ${computerWin}`
+results.appendChild(tally);
+
+const winner = document.createElement('p');
+winner.classList.add('winner');
+
+
+
 
     if(playerSelection === computerSelection) {
         console.log("Tie!");
@@ -44,22 +64,7 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-
-
-
-
-
-/*const btnRock = document.getElementById('btn1');
-const btnPaper = document.getElementById('btn2');
-const btnScissors = document.getElementById('btn3'); 
-
-btnRock.addEventListener('click', playRound);
-btnPaper.addEventListener('click', playRound);
-btnScissors.addEventListener('click', playRound); <------ Might be able to combine this into one event listener with a .forEach method/loop */ 
-
-//---------------------------------------------------------------------------------------------------------------
-
-/*----------------"rps-ui branch" removal
+/* NEED TO ADD THIS INTO FUNCTIONALITY-------
 function game() { 
     while (playerWin < 5 && computerWin < 5) {
         playRound();// <----------------------------playRound() function needs to be called for every loop
